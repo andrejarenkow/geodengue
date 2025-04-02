ed82b15fa50bde7843d8754adc6a6fd90e46e883
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2025-04-02 17:19:26 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,14 +5,15 @@ import streamlit as st
 # Configurações da página
 st.set_page_config(
     page_title="Geodengue",
-    page_icon="	:bug:",
+    page_icon=":bug:",
     layout="wide",
     initial_sidebar_state='expanded'
-) 
-col1, col2, col3 = st.columns([1,4,1])
+)
+
+col1, col2, col3 = st.columns([1, 4, 1])
 
 col1.image('https://github.com/andrejarenkow/csv/blob/master/logo_cevs%20(2).png?raw=true', width=100)
-col2.header('Coordenadas Notificacoes Dengue')
+col2.header('Coordenadas Notificações Dengue')
 col3.image('https://github.com/andrejarenkow/csv/blob/master/logo_estado%20(3)%20(1).png?raw=true', width=150)
 
 # Sidebar para upload do arquivo
@@ -23,10 +24,18 @@ if uploaded_file is not None:
     # Carregar o arquivo CSV
     df = pd.read_csv(uploaded_file)
 
-    # Centro
-    lat_center = (df['latitude'].max() + df['latitude'].min())/2
-    lon_center = (df['longitude'].max() + df['longitude'].min())/2
-    
+    # Criando filtro de município
+    municipio = st.selectbox(label='Selecione um município:', options=df['Municipio'].unique())
+    aplicar_filtro = st.button("Aplicar Filtro")
+
+    # Aplicar o filtro apenas quando o botão for clicado
+    if aplicar_filtro:
+        df = df[df['Municipio'] == municipio]
+
+    # Centro do mapa
+    lat_center = (df['latitude'].max() + df['latitude'].min()) / 2
+    lon_center = (df['longitude'].max() + df['longitude'].min()) / 2
+
     # Criar o mapa
     fig = px.scatter_mapbox(
         df,
@@ -35,44 +44,43 @@ if uploaded_file is not None:
         hover_name=df.columns[0],  # Nome da primeira coluna para exibição no hover
         zoom=5.5,
         mapbox_style="open-street-map",
-        center = {'lat':lat_center,'lon':lon_center},
-        height = 800,
-        width = 800
+        center={'lat': lat_center, 'lon': lon_center},
+        height=800,
+        width=800
     )
-    
+
     # Exibir o mapa
     st.plotly_chart(fig)
-    
+
     # Contar quantos valores NaN existem na coluna 'latitude' por município
     nan_por_municipio = df[df['latitude'].isna()].groupby('Municipio').size().reset_index(name='NaN_Latitude')
-    
+
     # Contar o total de registros por município
     total_por_municipio = df.groupby('Municipio').size().reset_index(name='Total_Registros')
-    
+
     # Unir os dois DataFrames
     resultado = pd.merge(nan_por_municipio, total_por_municipio, on='Municipio', how='right').fillna(0)
-    
+
     # Calcular a porcentagem de valores NaN
     resultado['% NaN'] = round((resultado['NaN_Latitude'] / resultado['Total_Registros']) * 100)
-    
+
     # Adicionar linha de total
     total_nan = resultado['NaN_Latitude'].sum()
     total_registros = resultado['Total_Registros'].sum()
     total_percentual = (total_nan / total_registros) * 100 if total_registros > 0 else 0
-    
+
     linha_total = pd.DataFrame({
         'Municipio': ['TOTAL'],
         'NaN_Latitude': [total_nan],
         'Total_Registros': [total_registros],
         '% NaN': [total_percentual]
     })
-    
+
     # Concatenar com o DataFrame original
     resultado = pd.concat([resultado, linha_total], ignore_index=True)
 
-    # renomeando as coluans
-    resultado.columns = ['Município','Não achados', 'Total notificações', 'Porcentagem']
-    
-    # Exibir o resultado
-    st.dataframe(resultado, hide_index = True)
+    # Renomeando as colunas
+    resultado.columns = ['Município', 'Não achados', 'Total notificações', 'Porcentagem']
 
+    # Exibir o resultado
+    st.dataframe(resultado, hide_index=True)
