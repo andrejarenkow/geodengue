8d4c94545e1da34ebade8594834939f0a67826a2
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2025-04-02 17:21:47 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,8 @@ if uploaded_file is not None:
     # Carregar o arquivo CSV
     df = pd.read_csv(uploaded_file)
 
-    # Criando filtro de município
+    # Criando filtro de município na sidebar
+    st.sidebar.header("Filtro de Município")
     municipio = st.sidebar.selectbox(label='Selecione um município:', options=df['Municipio'].unique())
     aplicar_filtro = st.sidebar.button("Aplicar Filtro")
 
