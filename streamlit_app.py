e632bb765bb34c1935c76fff3a114d8432c7aa34
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2025-04-04 15:54:43 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -172,31 +172,42 @@ if uploaded_file is not None:
 
     # Plotly event
     # Writes a component similar to st.write()
+    import pydeck as pdk
+    
+    # 2014 locations of car accidents in the UK
+    UK_ACCIDENTS_DATA = ('https://raw.githubusercontent.com/uber-common/'
+                         'deck.gl-data/master/examples/3d-heatmap/heatmap-data.csv')
+    
+    # Define a layer to display on a map
     layer = pdk.Layer(
-            "ScatterplotLayer",
-            data=df,
-            get_position='[longitude, latitude]',
-            get_fill_color="color",
-            get_radius=100,
-            pickable=True,
-            auto_highlight=True,
-            id="endereco",
-        )
+        'HexagonLayer',
+        UK_ACCIDENTS_DATA,
+        get_position=['lng', 'lat'],
+        auto_highlight=True,
+        elevation_scale=50,
+        pickable=True,
+        elevation_range=[0, 3000],
+        extruded=True,                 
+        coverage=1)
     
-    tooltip = {
-            "html": "<b>Município:</b> {Municipio}<br/>"
-                    "<b>Endereço:</b> {endereco}<br/>"
-                    "<b>Classificação:</b> {CLASSI_FIN}<br/>"
-                    "<b>Data:</b> {DT_SIN_PRI}",
-            "style": {"backgroundColor": "steelblue", "color": "white"}
-        }
+    # Set the viewport location
+    view_state = pdk.ViewState(
+        longitude=-1.415,
+        latitude=52.2323,
+        zoom=6,
+        min_zoom=5,
+        max_zoom=15,
+        pitch=40.5,
+        bearing=-27.36)
     
-    view_state = pdk.ViewState(latitude=lat_center, longitude=lon_center, zoom=10, pitch=0)
+    # Render
+    r = pdk.Deck(layers=[layer], initial_view_state=view_state)
+    #r.to_html('demo.html')
     
     r = pdk.Deck(layers=[layer], initial_view_state=view_state, tooltip=tooltip, map_style=pdk.map_styles.SATELLITE)
     
     event = st.pydeck_chart(r, selection_mode="single-object", on_select="rerun",)
-    event.selection['objects']['endereco'][0]
+
 
 
 
