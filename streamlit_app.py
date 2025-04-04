8ae119559f6b7974939762c020ebfbd93355cf8c
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2025-04-04 16:00:55 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -172,39 +172,7 @@ if uploaded_file is not None:
 
     # Plotly event
     # Writes a component similar to st.write()
-    import pydeck as pdk
-    
-    # 2014 locations of car accidents in the UK
-    UK_ACCIDENTS_DATA = ('https://raw.githubusercontent.com/uber-common/'
-                         'deck.gl-data/master/examples/3d-heatmap/heatmap-data.csv')
-    
-    # Define a layer to display on a map
-    layer = pdk.Layer(
-        'HexagonLayer',
-        UK_ACCIDENTS_DATA,
-        get_position=['lng', 'lat'],
-        auto_highlight=True,
-        elevation_scale=50,
-        pickable=True,
-        elevation_range=[0, 3000],
-        extruded=True,                 
-        coverage=1)
-    
-    # Set the viewport location
-    view_state = pdk.ViewState(
-        longitude=-1.415,
-        latitude=52.2323,
-        zoom=6,
-        min_zoom=5,
-        max_zoom=15,
-        pitch=40.5,
-        bearing=-27.36)
-    
-    # Render
-    r = pdk.Deck(layers=[layer], initial_view_state=view_state)
-    #r.to_html('demo.html')
-    
-    event = st.pydeck_chart(r, selection_mode="single-object", on_select="rerun",)
+
 
 
 
