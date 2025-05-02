1643bd07cbed3ef0acc39d226252979d7a07eb7d
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2025-05-02 16:02:01 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import plotly.express as px
 import plotly.figure_factory as ff  # Import necessário para hexbin
 import streamlit as st
 from streamlit_plotly_events import plotly_events
-import pydeck as pdkhttps://github.com/andrejarenkow/geodengue/blob/main/streamlit_app.py
+import pydeck as pdk
 
 # Configurações da página
 st.set_page_config(
